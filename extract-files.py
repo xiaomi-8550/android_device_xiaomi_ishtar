52a3f5d353d73e6875dfb49417a542077a81095b
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-03-23 17:55:59 +0530

    ishtar: Address missing SetTaskProfiles symbol in camera prebuilts
    This is because DECLS was removed in [1] on Android 15 QPR2, and
    caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles
    with libprocessgroup_shim.
    
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Change-Id: Ic46aae47bdf6be09736fe89d798cb7017f07cc6c

--- extract-files.py
+++ extract-files.py
@@ -57,6 +57,14 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/libxmi_high_dynamic_range_cdsp.so',
     ): blob_fixup()
         .strip_debug_sections(),
+    (
+        'odm/lib64/libcamxcommonutils.so',
+        'odm/lib64/libchifeature2.so',
+        'odm/lib64/libmialgoengine.so',
+        'odm/lib64/hw/camera.xiaomi.so',
+        'odm/lib64/hw/com.qti.chi.override.so',
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'odm/lib64/libwrapper_dlengine.so': blob_fixup()
         .add_needed('liblog.so'),
 }
